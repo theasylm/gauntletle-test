2357a2b1705e815987941909431dbec579f5782e
Shawn Kiewel <smkiewel@uga.edu>
2022-07-02 14:06:43 -0400

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build --base=/gauntletle/
+npm run build --base=/gauntletle-test/
 
 # navigate into the build output directory
 cd dist
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github-theasylm:theasylm/gauntletle.git main:gh-pages
+git push -f git@github-theasylm:theasylm/gauntletle-test.git main:gh-pages
 
 cd -
